2de90a14db75f935a3a610cdd053e6a421685aad
Joao4Pedroc <joao4pedroc@gmail.com>
2024-05-21 15:53:02 -0300

    Feat: website Home

--- the-wild-oasis-website/app/page.tsx
+++ the-wild-oasis-website/app/page.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import bg from "@/public/bg.png";
+// redo design on big screens
+
 export default function Home() {
   return (
-    <div>
-      <div>The Wild oasis. Welcome to paradise</div>
-    </div>
+    <main>
+      <Image
+        src={bg}
+        fill
+        className="object-cove object-topr"
+        placeholder="blur"
+        quality={80}
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 tracking-tight font-normal mb-10">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="text-lg py-6 bg-accent-500 px-8 font-semibold hover:bg-accent-600 transition-all text-primary-800"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
